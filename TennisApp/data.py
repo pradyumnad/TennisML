61197f3de2014d82965273d2993622a7da103edd
Prady <pradyumnadoddala@gmail.com>
2016-02-06 15:25:42 -0600

    Added normalised fields for all the values.

--- TennisApp/data.py
+++ TennisApp/data.py
@@ -1,9 +1,33 @@
+import re
 import urllib
 import csv
 
 from bs4 import BeautifulSoup
 
 __author__ = 'pradyumnad'
+
+
+def normalized(val1, val2):
+    if " " in val1:
+        parts1 = val1.split(" ")
+        parts2 = val2.split(" ")
+
+        if parts1[1] == "MPH" or parts1[1] == "KMH":
+            return parts1[0], parts2[0]
+        else:
+            m1 = re.search('([0-9])+(\s)*%', val1)
+            m2 = re.search('([0-9])+(\s)*%', val2)
+
+            tmp1 = int(m1.group(0).strip(" %"))
+            tmp2 = int(m2.group(0).strip(" %"))
+            return tmp1 / 100.0, tmp2 / 100.0
+    else:
+        val1_temp = float(val1)
+        val2_temp = float(val2)
+
+        val1norm = val1_temp / (val1_temp + val2_temp)
+        val2norm = val2_temp / (val1_temp + val2_temp)
+        return val1norm, val2norm
 
 
 def extract_match_details(match_url):
@@ -27,7 +51,7 @@ def extract_match_details(match_url):
     for div in stats_table.find_all(class_=['header', 'row']):
         parts = div.get_text(" | ", strip=True).split(" | ")
         # print(parts, len(parts))
-        if (len(parts) > 4):
+        if len(parts) > 4:
             field_names.append(parts[2])
             player1[parts[2]] = parts[0] + " | " + parts[1]
             player2[parts[2]] = parts[3] + " | " + parts[4]
@@ -35,6 +59,13 @@ def extract_match_details(match_url):
             field_names.append(parts[1])
             player1[parts[1]] = parts[0]
             player2[parts[1]] = parts[2]
+
+            if parts[1] != "Stats":
+                field_norm = parts[1] + " Norm"
+                field_names.append(field_norm)
+                val1, val2 = normalized(parts[0], parts[2])
+                player1[field_norm] = val1
+                player2[field_norm] = val2
 
     field_names.append("url")
     player1["url"] = match_url
@@ -61,11 +92,15 @@ def match_field_names(match_url):
     field_names = []
 
     for div in stats_table.find_all(class_=['header', 'row']):
+        field = ""
         parts = div.get_text(" | ", strip=True).split(" | ")
         if (len(parts) > 4):
-            field_names.append(parts[2])
+            field = parts[2]
         else:
-            field_names.append(parts[1])
+            field = parts[1]
+        field_names.append(field)
+        if field != "Stats":
+            field_names.append(field + " Norm")
 
     field_names.append("url")
 
@@ -97,18 +132,19 @@ def aus_open():
 def us_open():
     us_url = "http://www.usopen.org"
     usopen_urls = ["/en_US/scores/stats/day20/1701ms.html", "/en_US/scores/stats/day19/2701ms.html",
-                    "/en_US/scores/stats/day19/3601ms.html", "/en_US/scores/stats/day20/4601ms.html",
-                    "/en_US/scores/stats/day18/5501ms.html", "/en_US/scores/stats/day20/21601ms.html",
-                    "/en_US/scores/stats/day20/22601ms.html", "/en_US/scores/stats/day20/23501ms.html",
-                    "/en_US/scores/stats/day19/24501ms.html", "/en_US/scores/stats/day20/31301ms.html",
-                    "/en_US/scores/stats/day20/32201ms.html", "/en_US/scores/stats/day20/33301ms.html",
-                    "/en_US/scores/stats/day20/34201ms.html", "/en_US/scores/stats/day20/35401ms.html",
-                    "/en_US/scores/stats/day20/36101ms.html", "/en_US/scores/stats/day20/46201ms.html",
-                    "/en_US/scores/stats/day19/51301ms.html", "/en_US/scores/stats/day19/52301ms.html"]
+                   "/en_US/scores/stats/day19/3601ms.html", "/en_US/scores/stats/day20/4601ms.html",
+                   "/en_US/scores/stats/day18/5501ms.html", "/en_US/scores/stats/day20/21601ms.html",
+                   "/en_US/scores/stats/day20/22601ms.html", "/en_US/scores/stats/day20/23501ms.html",
+                   "/en_US/scores/stats/day19/24501ms.html", "/en_US/scores/stats/day20/31301ms.html",
+                   "/en_US/scores/stats/day20/32201ms.html", "/en_US/scores/stats/day20/33301ms.html",
+                   "/en_US/scores/stats/day20/34201ms.html", "/en_US/scores/stats/day20/35401ms.html",
+                   "/en_US/scores/stats/day20/36101ms.html", "/en_US/scores/stats/day20/46201ms.html",
+                   "/en_US/scores/stats/day19/51301ms.html", "/en_US/scores/stats/day19/52301ms.html"]
 
-    csvfile = open('data/us_open.csv', 'w')
     field_names = match_field_names('http://www.usopen.org/en_US/scores/stats/day20/1701ms.html')
     print(field_names)
+
+    csvfile = open('data/us_open.csv', 'w')
     writer = csv.DictWriter(csvfile, fieldnames=field_names)
     csv.writer(csvfile).writerow(field_names)
     for iurl in usopen_urls:
